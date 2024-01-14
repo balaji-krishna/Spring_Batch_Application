2ec36229006aeca975cd1ef87329b6dd7bf3d8ed
Administrator <Administrator@DESKTOP-27TFO05>
2024-01-14 17:34:05 +0530

    Code for job scheduler

---
+++ Spring_Batch_Application/src/main/java/com/springBatch/app/entity/MainChunkJobScheduler.java
@@ -0,0 +1,42 @@
+package com.springBatch.app.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainChunkJobScheduler {
+	
+	@Autowired
+	JobLauncher jobLauncher;
+	
+	@Qualifier("mainChunkJob")
+	@Autowired
+	Job mainChunkJob;
+
+	@Scheduled(cron = "0 0/1 * 1/1 * ?")
+	public void secondJobStarter() {
+		Map<String, JobParameter> params = new HashMap<>();
+		params.put("currentTime", new JobParameter(System.currentTimeMillis()));
+		
+		JobParameters jobParameters = new JobParameters(params);
+		
+		try {
+			JobExecution jobExecution = 
+					jobLauncher.run(mainChunkJob, jobParameters);
+			System.out.println("Job Execution ID = " + jobExecution.getId());
+		}catch(Exception e) {
+			System.out.println("Exception while starting job");
+		}
+	}
+
+}
