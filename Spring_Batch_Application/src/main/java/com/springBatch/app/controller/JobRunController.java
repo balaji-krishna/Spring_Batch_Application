384c160c1cbef1b27595e38e45db667cc82770f6
Administrator <Administrator@DESKTOP-27TFO05>
2024-01-14 17:25:44 +0530

    Code to stop job based on job execution id

--- Spring_Batch_Application/src/main/java/com/springBatch/app/controller/JobRunController.java
+++ Spring_Batch_Application/src/main/java/com/springBatch/app/controller/JobRunController.java
@@ -2,6 +2,7 @@ package com.springBatch.app.controller;
 
 import java.util.List;
 
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +20,24 @@ public class JobRunController {
 	@Autowired
 	private JobRunService jobRunService;
 	
+	@Autowired
+	JobOperator jobOperator;
+	
 	@GetMapping("/startJob/{jobName}")
 	public String startJob(@PathVariable String jobName, 
 			@RequestBody List<JobParametersRequest> jobParamsRequestList) throws Exception {
 		jobRunService.startJob(jobName, jobParamsRequestList);
 		return "The requested job is started";
+	}
+	
+	@GetMapping("/stop/{jobExecutionId}")
+	public String stopJob(@PathVariable long jobExecutionId) {
+		try {
+			jobOperator.stop(jobExecutionId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "The requested job is stopped";
 	}
 
 }
