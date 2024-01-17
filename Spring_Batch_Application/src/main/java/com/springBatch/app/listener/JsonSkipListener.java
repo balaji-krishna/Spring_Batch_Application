72183c6b41569715c5d6198de34e78c5e3c05d80
Administrator <Administrator@DESKTOP-27TFO05>
2024-01-17 15:40:45 +0530

    Added common entity for json chunk job

--- Spring_Batch_Application/src/main/java/com/springBatch/app/listener/JsonSkipListener.java
+++ Spring_Batch_Application/src/main/java/com/springBatch/app/listener/JsonSkipListener.java
@@ -7,34 +7,35 @@ import java.util.Date;
 import org.springframework.batch.core.SkipListener;
 import org.springframework.stereotype.Component;
 
-import com.springBatch.app.entity.CustomerJson;
+import com.springBatch.app.entity.Customer;
 
 @Component
-public class JsonSkipListener implements SkipListener<CustomerJson, CustomerJson> {
+public class JsonSkipListener implements SkipListener<Customer, Customer> {
 
 	@Override
 	public void onSkipInRead(Throwable t) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void onSkipInWrite(CustomerJson item, Throwable t) {
-		createFile("D:\\\\Eclipse\\\\FirstWorkspace\\\\Git\\\\Spring_Batch_Application\\\\Spring_Batch_Application\\\\OutputFiles\\\\SkippedBadJsonInputWrite.txt", 
+	public void onSkipInWrite(Customer item, Throwable t) {
+		createFile(
+				"D:\\\\Eclipse\\\\FirstWorkspace\\\\Git\\\\Spring_Batch_Application\\\\Spring_Batch_Application\\\\OutputFiles\\\\SkippedBadJsonInputWrite.txt",
 				item.toString());
 	}
 
 	@Override
-	public void onSkipInProcess(CustomerJson item, Throwable t) {
+	public void onSkipInProcess(Customer item, Throwable t) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public void createFile(String filePath, String data) {
-		try(FileWriter fileWriter = new FileWriter(new File(filePath), true)) {
+		try (FileWriter fileWriter = new FileWriter(new File(filePath), true)) {
 			fileWriter.write(data + "," + new Date() + "\n");
-		}catch(Exception e) {
-			
+		} catch (Exception e) {
+
 		}
 	}
 
